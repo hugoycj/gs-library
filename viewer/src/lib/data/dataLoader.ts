84229ac49385f154bd52cc2a66f454378819b2f7
hugoycj <576550489@qq.com>
2024-04-08 13:47:18 +0800

    Update data

--- viewer/src/lib/data/dataLoader.ts
+++ viewer/src/lib/data/dataLoader.ts
@@ -1,10 +1,27 @@
-import scenes from "$lib/data/scenes.json";
-import models from "$lib/data/models.json";
-
 export async function getScenes() {
-    return scenes;
-}
-
-export async function getModels() {
-    return models;
-}
+    try {
+      const response = await fetch('https://3dgs-benchmark.s3.amazonaws.com/scenes.json');
+      if (!response.ok) {
+        throw new Error(`HTTP error ${response.status}`);
+      }
+      const scenes = await response.json();
+      return scenes;
+    } catch (error) {
+      console.error('Error fetching scenes:', error);
+      throw error;
+    }
+  }
+  
+  export async function getModels() {
+    try {
+      const response = await fetch('https://3dgs-benchmark.s3.amazonaws.com/models.json');
+      if (!response.ok) {
+        throw new Error(`HTTP error ${response.status}`);
+      }
+      const models = await response.json();
+      return models;
+    } catch (error) {
+      console.error('Error fetching models:', error);
+      throw error;
+    }
+  }
